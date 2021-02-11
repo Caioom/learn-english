6cb9380c8db6b5cf13c516ee55f470c15e8b9e82
caioom <caio.ds.2003@gmail.com>
2021-02-10 22:06:35 -0300

    Project Startup

---
+++ src/main/kotlin/com/caio/englishlearn/EnglishLearnApplication.kt
@@ -0,0 +1,11 @@
+package com.caio.englishlearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnglishLearnApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnglishLearnApplication>(*args)
+}
